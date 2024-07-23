5769f1a2c01ad67f3bcf64b223896b544f9bec65
Leslie <1813180652@qq.com>
2024-07-23 22:49:48 +0800

    create project

---
+++ TMC222x.cpp
@@ -0,0 +1,22 @@
+#include "TMC222x.hpp"
+
+#include "stdint.h"
+
+class TMC222x
+{
+private:
+    uint8_t id;
+
+public:
+    TMC222x();
+    ~TMC222x();
+};
+
+TMC222x::TMC222x(/* args */)
+{
+}
+
+TMC222x::~TMC222x()
+{
+}
+
